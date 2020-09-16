00e290db468318e8f1cb2c54c8f838b019b9605a
gilbert <gilbert.baidya@gmail.com>
2020-09-16 14:41:41 -0700

    Updated at Bangladesh time

--- Cucumber-hello-world/src/test/java/skeleton/RunCukesTest.java
+++ Cucumber-hello-world/src/test/java/skeleton/RunCukesTest.java
@@ -10,6 +10,11 @@ public class RunCukesTest {
 	private void syso() {
 
 		String syso = "Test";
+		String t = "rashu";
+		String q = "Moni";
+		String f = "Chandu";
+		String w = "Modon";
+		String s = "Test";
 	
 	}
 }
